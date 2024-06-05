dd68cd9cb2ace1b1e352624198c887e5a3242b36
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-06-05 16:34:44 +0530

    login API

--- Server/controller/user.controller.js
+++ Server/controller/user.controller.js
@@ -1,3 +1,4 @@
+const userModel = require("../model/user.model");
 const UserService = require("../services/user.service");
 
 exports.register = async (req, res, next) => {
@@ -5,6 +6,35 @@ exports.register = async (req, res, next) => {
     const { email, password } = req.body;
     const successRes = await UserService.registerUser(email, password);
     res.json({ status: true, success: "User Registered Successfully." });
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserService.checkUser(email);
+
+    if (!user) {
+      throw new Error("User do not exist. Please Register.");
+    } else {
+      const isMatch = user.comparePassword(password);
+
+      if (isMatch === false) {
+        throw new Error("Invalid Password.");
+      } else {
+        let tokenData = { _id: user._id, email: user.email };
+        const token = await UserService.generateToken(
+          tokenData,
+          "todoease",
+          "1h"
+        );
+
+        res.status(200).json({ status: true, token: token });
+      }
+    }
   } catch (err) {
     throw err;
   }
