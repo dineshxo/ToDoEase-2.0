f11c0ed660f9b27bf4f2faeb3f44c1269c9dc1a3
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-06-05 15:37:36 +0530

    Registration connected.
    Registration connected with flutter UI.

--- todoease/lib/screens/login.dart
+++ todoease/lib/screens/login.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:todoease/config/config.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -15,26 +11,11 @@ class _LoginState extends State<Login> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  void registerUser() async {
-    if (passwordController.text.isNotEmpty && emailController.text.isNotEmpty) {
-      var resBody = {
-        "email": emailController.text,
-        "password": passwordController.text,
-      };
-
-      var response = await http.post(Uri.parse(registration),
-          body: jsonEncode(resBody),
-          headers: {"Content-Type": "application/json"});
-
-      print(response);
-    } else {}
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Register"),
+        title: const Text("Log In"),
       ),
       body: Column(
         children: [
@@ -46,11 +27,7 @@ class _LoginState extends State<Login> {
             controller: passwordController,
             decoration: const InputDecoration(hintText: "password"),
           ),
-          ElevatedButton(
-              onPressed: () {
-                registerUser();
-              },
-              child: const Text("Register"))
+          ElevatedButton(onPressed: () {}, child: const Text("Log In"))
         ],
       ),
     );
---
+++ todoease/lib/screens/registration.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:todoease/config/config.dart';
+import 'package:todoease/screens/login.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void registerUser() async {
+    if (passwordController.text.isNotEmpty && emailController.text.isNotEmpty) {
+      var resBody = {
+        "email": emailController.text,
+        "password": passwordController.text,
+      };
+
+      var response = await http.post(Uri.parse(registration),
+          body: jsonEncode(resBody),
+          headers: {"Content-Type": "application/json"});
+
+      var jsonResponse = jsonDecode(response.body);
+
+      print(jsonResponse['status']);
+
+      if (jsonResponse['status']) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Login()));
+      } else {
+        print('Registration unsuccessful.');
+      }
+    } else {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: emailController,
+            decoration: const InputDecoration(hintText: "email"),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: const InputDecoration(hintText: "password"),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                registerUser();
+              },
+              child: const Text("Register"))
+        ],
+      ),
+    );
+  }
+}
