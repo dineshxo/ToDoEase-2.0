27637e0a5dcd0a43311ba1c0fe6f4b6a649ed58a
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-06-08 11:40:05 +0530

    registration bug fixed

--- todoease/lib/screens/registration.dart
+++ todoease/lib/screens/registration.dart
@@ -7,130 +7,198 @@ import 'package:todoease/constants/styles.dart';
 import 'package:todoease/screens/login.dart';
 
 class Registration extends StatefulWidget {
-  const Registration({super.key});
+  const Registration({Key? key}) : super(key: key);
 
   @override
   State<Registration> createState() => _RegistrationState();
 }
 
 class _RegistrationState extends State<Registration> {
+  final _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController reEnterPasswordController = TextEditingController();
+
+  String? passwordError;
 
   void registerUser() async {
-    if (passwordController.text.isNotEmpty && emailController.text.isNotEmpty) {
-      var reqBody = {
-        "email": emailController.text,
-        "password": passwordController.text,
-      };
+    if (_formKey.currentState!.validate()) {
+      if (passwordController.text == reEnterPasswordController.text) {
+        var reqBody = {
+          "email": emailController.text,
+          "password": passwordController.text,
+        };
 
-      var response = await http.post(Uri.parse(registration),
+        var response = await http.post(
+          Uri.parse(registration),
           body: jsonEncode(reqBody),
-          headers: {"Content-Type": "application/json"});
+          headers: {"Content-Type": "application/json"},
+        );
 
-      var jsonResponse = jsonDecode(response.body);
+        var jsonResponse = jsonDecode(response.body);
 
-      print(jsonResponse['status']);
+        print(jsonResponse['status']);
 
-      if (jsonResponse['status']) {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const Login()));
+        if (jsonResponse['status']) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Login()),
+          );
+        } else {
+          print('Registration unsuccessful.');
+        }
       } else {
-        print('Registration unsuccessful.');
+        setState(() {
+          passwordError = "Passwords do not match.";
+        });
       }
-    } else {}
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Text(
-                "Welcome!",
-                style: TextStyle(
-                    color: Colors.orange[800],
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                "Let's be productive.",
-                style: TextStyle(
-                    color: const Color.fromARGB(255, 97, 97, 97),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Column(
-                children: [
-                  TextField(
-                    controller: emailController,
-                    decoration: textFieldDecoration,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration:
-                        textFieldDecoration.copyWith(hintText: "Password"),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Login()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Already have an Account",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.w600),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.orange[800],
-                      )
-                    ],
-                  )),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  registerUser();
-                },
-                style: elevatedButtonStyle,
-                child: const Row(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text("Register", style: buttonText),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                      size: 20,
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Welcome!",
+                      style: TextStyle(
+                          color: Colors.orange[800],
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      "Let's be productive.",
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 97, 97, 97),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          decoration: textFieldDecoration.copyWith(
+                            labelText: "Email",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your email';
+                            }
+
+                            if (!value.contains('@')) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: textFieldDecoration.copyWith(
+                            labelText: "Password",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your password';
+                            }
+
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters long';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          controller: reEnterPasswordController,
+                          obscureText: true,
+                          decoration: textFieldDecoration.copyWith(
+                            labelText: "Re-enter Password",
+                            errorText: passwordError, // Display error message
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please re-enter your password';
+                            }
+
+                            if (value != passwordController.text) {
+                              return 'Passwords do not match';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Login()),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Already have an Account",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.orange[800],
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      onPressed: registerUser,
+                      style: elevatedButtonStyle,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Register", style: buttonText),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                            size: 20,
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
